cc2e92879f1866bc59c75aa69bec634d4b31d33d
Rachel McQuiggan <55991096+rmcquigg@users.noreply.github.com>
2023-05-30 16:17:14 -0400

    Update geojson.js

--- assets/js/geojson.js
+++ assets/js/geojson.js
@@ -1,18 +1,18 @@
 var geojsonFeatures = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -75.5935425,
-                    39.26443303
-                ]
-            },
-           "type": "Feature",
-           "properties": {
-                "name": "Hc55-41",
-                "screen top": "107",
-                "screen bottom": "112"
-                "date installed": "12-Nov-1995"
-};
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Point",
+      "properties": {
+        "name": "Hc55-41",
+        "screen top": "107",
+        "screen bottom": "112",
+        "date installed": "12-Nov-1995"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [39.26443303, -75.5935425]
+      }
+    }
+  ]
+}
