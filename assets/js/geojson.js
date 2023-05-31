e7199df0dde307dabb99693378fd9d2d54f3d488
Rachel McQuiggan <55991096+rmcquigg@users.noreply.github.com>
2023-05-31 12:36:03 -0400

    Update geojson.js

--- assets/js/geojson.js
+++ assets/js/geojson.js
@@ -7,6 +7,10 @@ var wells = [{ "type": "FeatureCollection",
     { "type": "Feature",
       "geometry": {"type": "Point", "coordinates": [-75.5935425,39.26443303]},
       "properties": {"Network": "Real Time","DGSID":"Hc55-41","screen top":"107 ft","screen bottom":"112 ft","date installed":"12-Nov-1995"}
+      },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-75.59324449,39.26418873]},
+      "properties": {"Network": "Real Time","DGSID":"Hc55-40","screen top":"201 ft","screen bottom":"251 ft","date installed":"19-Dec-1997"}
       }
     ]
   }
